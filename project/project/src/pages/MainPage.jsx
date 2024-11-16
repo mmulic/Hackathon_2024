b5f74e17a7c7ab045dfbc4620d1a537a739c3528
mmulic <mmulic06@gmail.com>
2024-11-16 16:25:49 -0600

    first test of the main page

--- project/project/src/pages/MainPage.jsx
+++ project/project/src/pages/MainPage.jsx
@@ -1,3 +1,15 @@
 export default function MainPage() {
-  return <div className="bg-red-500 w-full h-screen">Main</div>;
+  return (
+    <div className="bg-red-500 w-full h-screen">
+      Main
+      <div>
+        <img 
+          src="https://fourpage-inbound.adpearance.com/images/uploads/ares/25895/seq_my23_0015_v001_egmwdzgkg4gybp4j0o1jghmcyjonhop.jpg" 
+          alt="Background" 
+          className="w-full h-auto"
+        />
+      </div>
+    </div>
+  );
+
 }
