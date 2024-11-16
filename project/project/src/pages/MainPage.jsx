33b746252c6db975493803401af1eba4aad87513
mmulic <mmulic06@gmail.com>
2024-11-16 17:33:44 -0600

    commit 1 of starting the transiting on main jsx

--- project/project/src/pages/MainPage.jsx
+++ project/project/src/pages/MainPage.jsx
@@ -5,9 +5,14 @@ export default function MainPage() {
         <img className="w-full h-screen"
           src="https://fourpage-inbound.adpearance.com/images/uploads/ares/25895/seq_my23_0015_v001_egmwdzgkg4gybp4j0o1jghmcyjonhop.jpg" 
           alt="Background" 
-        />
+        /><div>
           <img src="https://seeklogo.com/images/T/toyota-logo-F54DDDC736-seeklogo.com.png" alt="Child Image" class="absolute top-[calc(50%-500px)] left-[calc(50%-120px)] w-1/10 h-auto" />
+        
+          
+          <h2 class="absolute top-[calc(50%--500px)] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl ">Scroll down</h2>
 
-      </div> 
+        </div>
+      </div>
+      
   );
 }
