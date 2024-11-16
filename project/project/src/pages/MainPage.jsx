e3de04310f8fc03489bdf705de8ce0c7fb1f16e0
mmulic <mmulic06@gmail.com>
2024-11-16 16:57:25 -0600

    completed main page initial images

--- project/project/src/pages/MainPage.jsx
+++ project/project/src/pages/MainPage.jsx
@@ -1,15 +1,13 @@
+import '../index.css'; 
 export default function MainPage() {
   return (
-    <div className="bg-red-500 w-full h-screen">
-      Main
-      <div>
-        <img 
+      <div className="relative">
+        <img className="w-full h-screen"
           src="https://fourpage-inbound.adpearance.com/images/uploads/ares/25895/seq_my23_0015_v001_egmwdzgkg4gybp4j0o1jghmcyjonhop.jpg" 
           alt="Background" 
-          className="w-full h-auto"
         />
-      </div>
-    </div>
-  );
+          <img src="https://seeklogo.com/images/T/toyota-logo-F54DDDC736-seeklogo.com.png" alt="Child Image" class="absolute top-[calc(50%-500px)] left-[calc(50%-120px)] w-1/10 h-auto" />
 
+      </div> 
+  );
 }
