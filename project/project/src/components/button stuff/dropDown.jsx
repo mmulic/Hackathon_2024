07caf9ec8ed9aa5e7c42afab52a44cef00f9fc2a
omarMilan <131297774+omarMilan@users.noreply.github.com>
2024-11-16 19:48:51 -0600

    update

--- project/project/src/components/button stuff/dropDown.jsx
+++ project/project/src/components/button stuff/dropDown.jsx
@@ -1,8 +1,9 @@
 import Button from "./button";
+import "../../index.css";
 
 export default function DropDown({ text = "insert text" }) {
   return (
-    <Button className="w-40 h-10 bg-customGray shadow-sm shadow-black hover:shadow-none rounded-md transition-shadow duration-300">
+    <Button className="w-40 h-10 font-inria text-xl bg-customGray shadow-sm shadow-black hover:shadow-none rounded-md transition-shadow duration-300">
       {text}
     </Button>
   );
