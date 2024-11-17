911fa383fe9427915ad11332f081491e8d425f2d
omarel2amar <155870286+omarel2amar@users.noreply.github.com>
2024-11-17 00:39:14 -0600

    animations added to body
    added animations to photos along with labels

--- project/project/src/pages/InputPage.jsx
+++ project/project/src/pages/InputPage.jsx
@@ -1,11 +1,14 @@
 import Header from "../components/header";
 import Footer from "../components/footer";
+import Body from "../components/body";
 
 export default function InputPage() {
   return (
-    <div>
+    <div className="h-screen flex flex-col">
       <Header title="Choose your vehicle" />
+      <Body></Body>
       <Footer></Footer>
+      
     </div>
   );
 }
