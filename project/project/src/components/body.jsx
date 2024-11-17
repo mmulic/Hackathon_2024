52232f3fe4923691c40427c0dfbb81a7f7bc0117
omarel2amar <155870286+omarel2amar@users.noreply.github.com>
2024-11-16 23:20:40 -0600

    input body
    created body portion of input page with images

--- project/project/src/components/body.jsx
+++ project/project/src/components/body.jsx
@@ -1,8 +1,39 @@
-export default function Body(){
+import landcruiser from "../assets/landCruiser.webp";
+import camry from "../assets/toyotaCamry.jpg";
+import tundra from "../assets/tundraForest.jpg";
+import crown from "../assets/toyotaCrown.webp";
+
+export default function Body() {
     return (
-        <img 
-        className="absolute ml-4 justify-start items-center"
-        src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.outsideonline.com%2Foutdoor-gear%2Fcars-trucks%2Fyou-should-buy-2024-toyota-land-cruiser%2F&psig=AOvVaw27pNs1bt2OiM5f5jN9tsSo&ust=1731880481825000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPDL59jr4YkDFQAAAAAdAAAAABAE"
-        />
-    )
+        <div className="ml-10 top-10 relative overflow-x-hidden ">
+            {/* Flex container for the first row of images */}
+            <div className="flex gap-20 items-start ">
+                <img 
+                    className="rounded-[10px] w-[406px] object-contain"
+                    src={landcruiser}
+                    alt="Land Cruiser"
+                />
+                
+                <img 
+                    className="rounded-[10px] w-[346px] object-contain"
+                    src={camry}
+                    alt="Toyota Camry"
+                />
+                <img 
+                    className="rounded-[10px] w-[422px] object-contain"
+                    src={tundra}
+                    alt="Tundra"
+                />
+            </div>
+
+            {/* Separate container for the Toyota Crown */}
+            <div className="flex-col relative mt-14 left-[450px]  ">
+                <img 
+                    className="rounded-[10px] w-[422px] object-contain"
+                    src={crown}
+                    alt="Toyota Crown"
+                />
+            </div>
+        </div>
+    );
 }
